6c453937f653a84868eef24be4b8e1797e07b9f6
Magnus Brevik <mbremyk@hotmail.no>
2023-06-03 01:40:16 +0200

    Add different height and width to board Add flags and flagging Add failure on revealing a bomb Add board reset on failure

--- board.cpp
+++ board.cpp
@@ -18,9 +18,13 @@ public:
   const int default_size = 5;
 
 private:
-  int m_size = default_size;
+  int m_width = default_size;
+  int m_height = default_size;
   std::vector<std::vector<int>> m_board;
-  std::vector<std::vector<bool>> m_revealed;
+
+  enum state { HIDDEN, FLAGGED, REVEALED };
+
+  std::vector<std::vector<state>> m_state;
 
 public:
   Board() { init(); }
@@ -30,22 +34,36 @@ public:
     init();
   }
 
+  Board(int width, int height) {
+    set_size(width, height);
+    init();
+  }
+
   bool set_size(int size) {
-    m_size = std::min(size, MAX_SIZE);
+    m_width = std::min(size, MAX_SIZE);
+    m_height = std::min(size, MAX_SIZE);
     return false;
   }
 
-  int size() { return m_size; }
+  bool set_size(int width, int height) {
+    m_width = std::min(width, MAX_SIZE);
+    m_height = std::min(height, MAX_SIZE);
+    return false;
+  }
+
+  int width() const { return m_width; }
+
+  int height() const { return m_height; }
 
   bool init() {
     m_board.clear();
-    m_revealed.clear();
-    for (auto i = 0; i < m_size; ++i) {
+    m_state.clear();
+    for (auto i = 0; i < m_height; ++i) {
       auto &board_row = m_board.emplace_back();
-      auto &revealed_row = m_revealed.emplace_back();
-      for (auto j = 0; j < m_size; ++j) {
+      auto &revealed_row = m_state.emplace_back();
+      for (auto j = 0; j < m_width; ++j) {
         board_row.push_back(0);
-        revealed_row.push_back(false);
+        revealed_row.push_back(HIDDEN);
       }
     }
 
@@ -53,40 +71,42 @@ public:
   }
 
   bool init(int size) { return set_size(size) || init(); }
+  bool init(int width, int height) { return set_size(width, height) || init(); }
 
   bool populate_mines(int amount) {
-    std::vector<int> temp(m_size * m_size);
+    std::vector<int> temp(m_width * m_height);
     std::iota(temp.begin(), temp.end(), 0);
     std::shuffle(temp.begin(), temp.end(),
                  std::default_random_engine(std::time(nullptr)));
+    amount = std::min(amount, (m_width - 1) * (m_height - 1));
     int i = 0;
     while (i < amount && temp.size() > 0) {
       auto pos = temp.back();
       temp.pop_back();
-      m_board[pos / m_size][pos % m_size] = -1;
+      m_board[pos / m_width][pos % m_width] = -1;
       ++i;
     }
 
-    for (auto i = 0; i < m_size; ++i) {
-      for (auto j = 0; j < m_size; ++j) {
-        if (m_board[i][j] < 0)
+    for (auto y = 0; y < m_height; ++y) {
+      for (auto x = 0; x < m_width; ++x) {
+        if (m_board[y][x] < 0)
           continue;
-        if (i && j && m_board[i - 1][j - 1] < 0)
-          ++m_board[i][j];
-        if (i && m_board[i - 1][j] < 0)
-          ++m_board[i][j];
-        if (i && j < m_size - 1 && m_board[i - 1][j + 1] < 0)
-          ++m_board[i][j];
-        if (j && m_board[i][j - 1] < 0)
-          ++m_board[i][j];
-        if (j < m_size - 1 && m_board[i][j + 1] < 0)
-          ++m_board[i][j];
-        if (i < m_size - 1 && j && m_board[i + 1][j - 1] < 0)
-          ++m_board[i][j];
-        if (i < m_size - 1 && m_board[i + 1][j] < 0)
-          ++m_board[i][j];
-        if (i < m_size - 1 && j < m_size - 1 && m_board[i + 1][j + 1] < 0)
-          ++m_board[i][j];
+        if (y && x && m_board[y - 1][x - 1] < 0)
+          ++m_board[y][x];
+        if (y && m_board[y - 1][x] < 0)
+          ++m_board[y][x];
+        if (y && x < m_width - 1 && m_board[y - 1][x + 1] < 0)
+          ++m_board[y][x];
+        if (x && m_board[y][x - 1] < 0)
+          ++m_board[y][x];
+        if (x < m_width - 1 && m_board[y][x + 1] < 0)
+          ++m_board[y][x];
+        if (y < m_height - 1 && x && m_board[y + 1][x - 1] < 0)
+          ++m_board[y][x];
+        if (y < m_height - 1 && m_board[y + 1][x] < 0)
+          ++m_board[y][x];
+        if (y < m_height - 1 && x < m_width - 1 && m_board[y + 1][x + 1] < 0)
+          ++m_board[y][x];
       }
     }
 
@@ -94,46 +114,62 @@ public:
   }
 
   int reveal(int x, int y, bool recursed = false) {
-    if (m_revealed[x][y]) {
+    if (m_state[y][x]) {
       return 0;
     }
-    m_revealed[x][y] = true;
-    if (!recursed && m_board[x][y] < 0) {
+    m_state[y][x] = REVEALED;
+    if (!recursed && m_board[y][x] < 0) {
       return -1;
     }
-    if (m_board[x][y] > 0) {
+    if (m_board[y][x] > 0) {
       return 1;
     }
 
     int g = 0;
     if (x && y)
-      reveal(x - 1, y - 1, true);
+      g += reveal(x - 1, y - 1, true);
     if (x)
-      reveal(x - 1, y, true);
-    if (x && y < m_size - 1)
-      reveal(x - 1, y + 1, true);
+      g += reveal(x - 1, y, true);
+    if (x && y < m_height - 1)
+      g += reveal(x - 1, y + 1, true);
     if (y)
-      reveal(x, y - 1, true);
-    if (y < m_size - 1)
-      reveal(x, y + 1, true);
-    if (x < m_size - 1 && y)
-      reveal(x + 1, y - 1, true);
-    if (x < m_size - 1)
-      reveal(x + 1, y, true);
-    if (x < m_size - 1 && y < m_size - 1)
-      reveal(x + 1, y + 1, true);
-    return false;
+      g += reveal(x, y - 1, true);
+    if (y < m_height - 1)
+      g += reveal(x, y + 1, true);
+    if (x < m_width - 1 && y)
+      g += reveal(x + 1, y - 1, true);
+    if (x < m_width - 1)
+      g += reveal(x + 1, y, true);
+    if (x < m_width - 1 && y < m_height - 1)
+      g += reveal(x + 1, y + 1, true);
+    return g;
+  }
+
+  bool set_flag(int x, int y) {
+    if (m_state[y][x] == HIDDEN) {
+      m_state[y][x] = FLAGGED;
+      return false;
+    }
+    return true;
+  }
+
+  bool unset_flag(int x, int y) {
+    if (m_state[y][x] == FLAGGED) {
+      m_state[y][x] = HIDDEN;
+      return false;
+    }
+    return true;
   }
 
   std::string to_string(bool revealed = false) {
-    int indent = std::floor(std::log10(m_size));
+    int indent = std::floor(std::log10(m_height));
     int top_indent = indent;
     std::string o;
     o += "     ";
     for (auto i = 0; i < top_indent; ++i) {
       o += " ";
     }
-    for (auto i = 0; i < m_size; ++i) {
+    for (auto i = 0; i < m_width; ++i) {
       o += 'A' + i;
       o += "  ";
     }
@@ -143,14 +179,14 @@ public:
       o += " ";
     }
     o += TOP_LEFT_CORNER;
-    for (auto i = 0; i < m_size; ++i) {
+    for (auto i = 0; i < m_width; ++i) {
       o += HORIZONTAL_LINE;
       o += HORIZONTAL_LINE;
       o += HORIZONTAL_LINE;
     }
     o += TOP_RIGHT_CORNER;
     o += '\n';
-    for (auto i = 0; i < m_size; ++i) {
+    for (auto i = 0; i < m_height; ++i) {
       auto &row = m_board.at(i);
       o += " ";
       indent = top_indent - std::floor(std::log10(i + 1));
@@ -160,11 +196,18 @@ public:
       o += std::to_string(i + 1);
       o += " ";
       o += VERTICAL_LINE;
-      for (auto j = 0; j < m_size; ++j) {
+      for (auto j = 0; j < m_width; ++j) {
         auto val = row.at(j);
-        o += revealed || m_revealed[i][j] ? (val < 0 ? "" : " ") : " ";
-        o += revealed || m_revealed[i][j] ? std::to_string(val) : "_";
-        o += " ";
+
+        if(revealed || (m_state[i][j] == REVEALED)) {
+          o += val < 0 ? "" : " ";
+          o += std::to_string(val);
+          o += " ";
+        } else if (m_state[i][j] == FLAGGED) {
+          o += " 🚩";
+        } else {
+          o += " _ ";
+        }
       }
       o += VERTICAL_LINE;
       o += '\n';
@@ -174,7 +217,7 @@ public:
       o += " ";
     }
     o += BOTTOM_LEFT_CORNER;
-    for (auto i = 0; i < m_size; ++i) {
+    for (auto i = 0; i < m_width; ++i) {
       o += HORIZONTAL_LINE;
       o += HORIZONTAL_LINE;
       o += HORIZONTAL_LINE;
--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,24 +1,59 @@
 #include "board.cpp"
 #include <iostream>
-#include <random>
-#include <string>
 
-int size;
-int mines;
+enum flag_ops { NONE, SET, UNSET };
 
 int main() {
-  std::cin >> size >> mines;
-  Board board(size);
+  int width, height, mines;
+  flag_ops flag_op;
+  std::cout << "Width: ";
+  std::cin >> width;
+  std::cout << "Height: ";
+  std::cin >> height;
+  std::cout << "Mines: ";
+  std::cin >> mines;
+  Board board(width, height);
   board.populate_mines(mines);
-  printf("%s\n", board.to_string().c_str());
+  std::cout << board.to_string() << std::endl;
   std::string input;
   std::cin >> input;
   while (input.compare("quit")) {
-    int first = input[0] - 'A';
-    int last = std::stoi(input.substr(1, input.length())) - 1;
-    board.reveal(last, first);
+    if (input[0] == '+') {
+      flag_op = SET;
+    } else if (input[0] == '-') {
+      flag_op = UNSET;
+    } else {
+      flag_op = NONE;
+    }
+    int first = input[flag_op ? 1 : 0] - 'A';
+    int last = std::stoi(input.substr(flag_op ? 2 : 1, input.length())) - 1;
+    switch (flag_op) {
+    case SET:
+      board.set_flag(first, last);
+      break;
+    case UNSET:
+      board.unset_flag(first, last);
+      break;
+    case NONE:
+    default:
+      int rev = board.reveal(first, last);
+      printf("%d\n", rev);
+      if (rev < 0) {
+        std::cout << "Boom you ded" << std::endl;
+        std::cout << board.to_string(true) << std::endl;
+        std::cout << "Width: ";
+        std::cin >> width;
+        std::cout << "Height: ";
+        std::cin >> height;
+        std::cout << "Mines: ";
+        std::cin >> mines;
+        board.init(width, height);
+        board.populate_mines(mines);
+      }
+      break;
+    }
 
-    printf("%s\n", board.to_string().c_str());
+    std::cout << board.to_string() << std::endl;
     std::cin >> input;
   }
 }
